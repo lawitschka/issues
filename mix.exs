7c25ae55257c32e6f7530a64f3729c0b2fdabf5e
Moritz Lawitschka <me@moritzlawitschka.de>
2017-04-17 18:41:14 +0930

    Add documentation dependencies

--- mix.exs
+++ mix.exs
@@ -3,9 +3,11 @@ defmodule Issues.Mixfile do
 
   def project do
     [app: :issues,
-     escript: escript_config(),
+     name: "Issues",
      version: "0.1.0",
      elixir: "~> 1.4",
+     source_url: "https://github.com/lawitschka/issues",
+     escript: escript_config(),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
@@ -30,8 +32,10 @@ defmodule Issues.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      httpoison: "~> 0.9",
-      poison: "~> 2.2"
+      { :httpoison, "~> 0.9" },
+      { :poison,    "~> 2.2" },
+      { :ex_doc,    "~> 0.12" },
+      { :earmark,   "~> 1.0", override: true }
     ]
   end
 
